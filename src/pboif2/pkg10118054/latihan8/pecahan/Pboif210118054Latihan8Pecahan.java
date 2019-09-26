244e71abcef72700b301cba68c51bc00fd7783f6
farhan3211 <55830838+farhan3211@users.noreply.github.com>
2019-09-26 16:21:29 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118054/latihan8/pecahan/Pboif210118054Latihan8Pecahan.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118054.latihan8.pecahan;
+
+/**
+ *
+ * @author ASUS
+ * nama  :Farhan Milardi
+ * kelas :IF-2 
+ * nim   :10118054 
+ */
+public class Pboif210118054Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     * float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
